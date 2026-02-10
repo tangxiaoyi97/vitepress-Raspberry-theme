d17649b1865819f655b931061822cb2f283893bb
Harvey-txy <lingmaozzzero@hotmail.com>
2026-02-10 19:15:24 +0100

    init

---
+++ index.ts
@@ -0,0 +1,23 @@
+import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import { useData } from 'vitepress'
+import './style.css'
+import type { Theme } from 'vitepress'
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => {
+        const { frontmatter } = useData()
+        
+        if (frontmatter.value.title) {
+          return h('div', { class: 'newspaper-headline' }, frontmatter.value.title)
+        }
+      }
+    })
+  },
+
+  enhanceApp({ app }) {
+  }
+} satisfies Theme
